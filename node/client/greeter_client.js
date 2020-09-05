019840435a0ddc16c407cf3362da1c6384f38c8f
unknown <cristian.lopez04@uptc.edu.co>
2020-09-05 11:50:01 -0500

    Se realiza trabajo para fragmentar los protocolos de buffer

--- node/client/greeter_client.js
+++ node/client/greeter_client.js
@@ -4,7 +4,6 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const port = 3000;
 
-var PROTO_PATH = __dirname + '/../../protos/helloworld.proto';
 
 var grpc = require('grpc');
 var protoLoader = require('@grpc/proto-loader');
@@ -35,8 +34,7 @@ app.post('/sendNumber', (req, res)=>{
 });
 
 function main() {
-  var client = new hello_proto.Greeter('localhost:50051',
-                                       grpc.credentials.createInsecure());
+  var client = new hello_proto.Greeter('localhost:50051', grpc.credentials.createInsecure());
   var user;
   if (process.argv.length >= 3) {
     user = process.argv[2];
